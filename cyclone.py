46ff5a880ddea189253a27b049686e65ac79f84b
Smriti Singh <thesmritisingh11@gmail.com>
2026-04-20 22:25:38 +0530

    Fix input prompt for height in cyclone.py

--- cyclone.py
+++ cyclone.py
@@ -1,4 +1,4 @@
- height= float(input("enter your height"))
+height= float(input("enter your height"))
  credits=int(input("enter your credits"))
  if height>5 and credits>5:
   print("You are not tall enough to ride.")
