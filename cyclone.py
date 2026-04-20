a204ced4b08758df944fba4ec71e3aae757cf8f5
Smriti Singh <thesmritisingh11@gmail.com>
2026-04-20 22:25:57 +0530

    Fix indentation in cyclone.py

--- cyclone.py
+++ cyclone.py
@@ -1,5 +1,5 @@
 height= float(input("enter your height"))
- credits=int(input("enter your credits"))
+credits=int(input("enter your credits"))
  if height>5 and credits>5:
   print("You are not tall enough to ride.")
 elif height<5 and credits>5:
